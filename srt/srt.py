b697d3b15bbedaa33dc3f21da104dfa7c69da8e9
Hyukjin Kwon <124764622+khj3437@users.noreply.github.com>
2023-09-14 15:43:11 +0900

    Update srt.py

--- srt/srt.py
+++ srt/srt.py
@@ -7,19 +7,20 @@ import os
 # 비디오 파일 폴더 위치
 video_folder = "./"
 
-# 자막 파일 위치 
+# 자막 파일 폴더 위치 
 subtitle_folder = "./srt"
 
 # 비디오 목록 추출
 video_files = [f for f in os.listdir(video_folder) if f.endswith(('.mp4', '.mkv', '.avi'))]
 
-# 비디오 목록 길이 
+# 비디오 목록 길이 출력
 print(f'비디오파일 {len(video_files)}개 발견')
 
-# 자막 목록 추출
+# 자막 목록 추출 
 subtitle_files = [f for f in os.listdir(subtitle_folder) if f.endswith('.srt')]
-print(f'자막파일 {len(subtitle_files)}개 발견')
 
+# 자막 목록 길이 출력
+print(f'자막파일 {len(subtitle_files)}개 발견')
 
 if(len(video_files) == len(subtitle_files)):
     print("비디오와 자막 파일 개수 일치")
